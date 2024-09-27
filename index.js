bd87ecedf0498e9decb4f6353752a14b061cda05
ThuanHT <46106596+thuanyg@users.noreply.github.com>
2024-09-27 13:07:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080;
 
+// /!\ Bind the router db to the app
+server.db = router.db;
+
+// You must apply the auth middleware before the router
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server with Auth is running on port ${port}`);
+});
